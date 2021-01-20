06c7c8e1403ae604fb146a98e6bd8e7a7d0c10e3
Yari Andres <yari.andres@gmail.com>
2021-01-20 22:26:00 +0100

    adding delete function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function fetchTodos() {
 		.then(renderTodos);
 }
 
-const todoCollection = document.querySelector('.todo-collection');
+const todoCollection = document.getElementById('todo-collection');
 
 function renderTodos(todos) {
 	todoCollection.innerHTML = '';
@@ -14,7 +14,7 @@ function renderTodos(todos) {
 			<div class="card" data-id="${todo.id}">
 				<h2>${todo.title}</h2>
 				<button class="btn btn-edit">edit</button>
-				<button class="btn btn-delete">Delete</button>
+				<button type="button" class="btn btn-delete">Delete</button>
 			</di>
 		`;
 	});
@@ -33,76 +33,27 @@ addTodoForm.addEventListener('submit', function (event) {
 		body: JSON.stringify({
 			id: Math.floor(Math.random() * Math.floor(10000)),
 			title: `${event.target.todo.value}`,
-			completed: false
+			completed: false,
 		}),
 	})
 		.then(resp => resp.json())
 		.then(renderTodos)
-		.catch(err => console.log(err))
+		.catch(err => console.log(err));
+});
+
+todoCollection.addEventListener('click', function (event) {
+
+	let deleteButton = event.target.className === 'btn btn-delete';
+
+	if (deleteButton) {
+		let id = event.target.parentElement.dataset.id;
+
+		fetch(`http://localhost:3000/todos/${id}`, {
+			method: 'DELETE',
+		})
+			.then(response => response.json())
+			.then(fetchTodos);
+	}
 });
 
 fetchTodos();
-
-// DOM selectors
-// const todoGroup = document.querySelector('.todo-group');
-// const userInput = document.querySelector('.user-input');
-// const postButton = document.querySelector('.btn-submit');
-
-// // Event listeners
-// postButton.addEventListener('click', createTodo);
-
-// let todoArray = [];
-
-// window.onload = async function () {
-// 	const fetchUrl = 'http://localhost:3000/todos';
-
-// 	const response = await fetch(fetchUrl);
-// 	todoArray = await response.json();
-
-// 	todoArray.map(todo => {
-// 		const ul = document.createElement('ul');
-// 		const li = document.createElement('li');
-// 		li.classList.add('list-item');
-
-// 		const pTag = document.createElement('p');
-// 		pTag.classList.add('list-link');
-// 		pTag.textContent = todo.title;
-
-// 		const buttonEdit = document.createElement('a');
-// 		buttonEdit.setAttribute('href', '#');
-
-// 		buttonEdit.classList.add('list-button', 'ui-yell');
-// 		buttonEdit.textContent = 'Edit';
-
-// 		const deleteButton = document.createElement('button');
-// 		deleteButton.setAttribute('class', 'deleteBtn');
-// 		deleteButton.setAttribute('onclick', 'deleteTodo(event)');
-// 		deleteButton.textContent = 'Delete';
-// 		deleteButton.classList.add('list-button', 'ui-red');
-
-// 		li.appendChild(pTag);
-// 		li.appendChild(buttonEdit);
-// 		li.appendChild(deleteButton);
-
-// 		ul.appendChild(li);
-// 		todoGroup.appendChild(ul);
-// 	});
-// };
-
-// function createTodo(event) {
-// 	event.preventDefault();
-
-// 	const newTodo = {
-// 		id: Math.floor(Math.random() * Math.floor(10000)),
-// 		title: userInput.value,
-// 		completed: false,
-// 	};
-
-// 	const postUrl = 'http://localhost:3000/todos';
-
-//
-
-// function deleteTodo(event) {
-// 	let id = event;
-// 	console.log(id)
-// };
