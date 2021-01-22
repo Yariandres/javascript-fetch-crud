7bd5fbaa6b35bba6786e931165bed9b667316545
Yari Andres <yari.andres@gmail.com>
2021-01-22 21:36:55 +0100

    fetching todos

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
+window.onload = async () => {
+    const url = 'http://localhost:3000/todos';
+    let currentEvents = document.querySelector('#currentTodos');
 
+    try {
+        let response = await fetch(url);
+        let todos = await response.json();
+
+
+        if(todos.length > 0) {
+            todos.forEach(todo => {
+            
+                let listItem = document.createElement('li');
+                listItem.classList.add('listItem');
+                listItem.innerHTML = `
+                    <span>${todo.title}</span>
+                    <span>${todo.completed}</span>
+                    <span>
+                        <a href="details.html?id${todo.id}">View details</a>
+                    </span>
+                `;
+                currentEvents.appendChild(listItem)
+            }) 
+        } else {
+            currentEvents.innerHTML = 'No planned todos...'
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
